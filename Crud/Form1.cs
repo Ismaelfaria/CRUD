050278fe8d237575d4483c36b1acca92e8baf4ea
limaismael8901 <limaismael8901@gmail.com>
2023-11-22 06:15:33 -0300

    estudei ate o vetor.

--- Crud/Form1.cs
+++ Crud/Form1.cs
@@ -1,4 +1,5 @@
-﻿using MySql.Data.MySqlClient;
+﻿using Microsoft.Win32.SafeHandles;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -58,15 +59,11 @@ namespace Crud
                 conn = new MySqlConnection(sql);
                 conn.Open();
 
-                var query = "'%"+txtBuscar.Text+"%'";
-
-                var comandSelect = "SELECT * FROM contatos WHERE nome LIKE " + query +
-                    " OR email LIKE "+query;
-
+                var query = "'%"+txtBuscar.Text+ "%'";
+                var comandSelect = "SELECT * FROM contato WHERE nome LIKE " + query +
+                                   " OR email LIKE " + query;
                 MySqlCommand commandSelect = new MySqlCommand(comandSelect, conn);
-
                 MySqlDataReader reader = commandSelect.ExecuteReader();
-
                 listaContatos.Items.Clear();
 
                 while (reader.Read())
