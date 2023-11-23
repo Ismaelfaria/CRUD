3a1d00974cc9e8af21a2477436433818b84f8635
limaismael8901 <limaismael8901@gmail.com>
2023-11-23 04:37:15 -0300

    implemenntação do button Buscar

--- Crud/Form1.cs
+++ Crud/Form1.cs
@@ -20,6 +20,17 @@ namespace Crud
         public Form1()
         {
             InitializeComponent();
+
+            listaContatos.View = View.Details;
+            listaContatos.LabelEdit = true;
+            listaContatos.AllowColumnReorder = true;
+            listaContatos.FullRowSelect = true;
+            listaContatos.GridLines = true;
+
+            listaContatos.Columns.Add("Id",30, HorizontalAlignment.Left);
+            listaContatos.Columns.Add("Nome",150, HorizontalAlignment.Left);
+            listaContatos.Columns.Add("E-mail",150, HorizontalAlignment.Left);
+            listaContatos.Columns.Add("Telefone",150, HorizontalAlignment.Left);
         }
 
         private void SalvarContato_Click(object sender, EventArgs e)
@@ -57,22 +68,36 @@ namespace Crud
             try
             {
                 conn = new MySqlConnection(sql);
+
                 conn.Open();
 
-                var query = "'%"+txtBuscar.Text+ "%'";
-                var comandSelect = "SELECT * FROM contato WHERE nome LIKE " + query +
-                                   " OR email LIKE " + query;
+                string query = "'%"+txtBuscar.Text+ "%'";
+
+                string comandSelect = "SELECT * FROM contato WHERE nome LIKE " + query
+                    + " OR email LIKE " + query;
+
                 MySqlCommand commandSelect = new MySqlCommand(comandSelect, conn);
+
                 MySqlDataReader reader = commandSelect.ExecuteReader();
+
                 listaContatos.Items.Clear();
 
                 while (reader.Read())
                 {
                     string[] row =
                     {
-
+                        reader.GetString(0),
+                        reader.GetString(1),
+                        reader.GetString(2),
+                        reader.GetString(3),
                     };
-                };
+
+                    var linha_listView = new ListViewItem(row);
+
+                    listaContatos.Items.Add(linha_listView);
+                }
+
+
             }
             catch (Exception ex)
             {
